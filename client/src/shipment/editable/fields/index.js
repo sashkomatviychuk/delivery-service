463e6fed5447c142e41972c4ab4ce73d83afa0c6
sashkomatviychuk <sashko1921@gmail.com>
2018-08-03 22:06:28 +0200

    Remove fields

--- client/src/shipment/editable/fields/index.js
+++
@@ -1,5 +0,0 @@
-export { default as datetimeInput } from './datetimeInput';
-export { default as numberInput } from './numberInput';
-export { default as radioInput } from './radioInput';
-export { default as selectBiker } from './selectBiker';
-export { default as textInput } from './textInput';
